eb7c42cd838ad0089e06d575ae82bd37ca55c3ad
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-05-01 23:03:01 +0400

    ICollection<T> - определяет методы, используемые для управления универсальными коллекциями.

---
+++ ICollectionIList_02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICollectionIList_02
+{
+    class Program
+    {
+        static void Main()
+        {
+            var collection = new UserCollection<int> { 1, 2, 3 };
+
+            foreach (var item in collection)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(collection.Contains(2));
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
