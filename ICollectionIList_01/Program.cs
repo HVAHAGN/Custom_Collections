726f051f75fb59cfeb047574eee4f0d4af38ab6d
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-05-01 22:53:50 +0400

    Копирует элементы ICollection в Array, начиная с конкретного индекса Array.

--- ICollectionIList_01/Program.cs
+++ ICollectionIList_01/Program.cs
@@ -8,8 +8,28 @@ namespace ICollectionIList_01
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            var collection = new UserCollection();
+
+            foreach (var element in collection)
+            {
+                Console.WriteLine(element);
+            }
+
+            Console.WriteLine(new string('-', 2));
+
+            var array = new object[collection.Count];
+
+            collection.CopyTo(array, 0);
+
+            foreach (int element in array)
+            {
+                Console.WriteLine(element);
+            }
+
+            // Delay.
+            Console.ReadKey();
         }
     }
 }
