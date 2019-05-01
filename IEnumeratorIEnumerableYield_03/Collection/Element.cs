48b5014c96d0c6a5e8567875ad79b4e16f91eaec
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-05-01 22:35:21 +0400

    Создание простой пользовательской коллекции с использованием оператора yield.

---
+++ IEnumeratorIEnumerableYield_03/Collection/Element.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnumeratorIEnumerableYield_03.Collection
+{
+    // Создание простой пользовательской коллекции с использованием оператора yield.
+    // Класс, представляющий собой содержимое коллекции.
+    class Element
+    {
+        public int FieldA { get; set; }
+        public int FieldB { get; set; }
+
+        public Element(int fieldA, int fieldB)
+        {
+            FieldA = fieldA;
+            FieldB = fieldB;
+        }
+    }
+}
---
+++ IEnumeratorIEnumerableYield_03/Collection/UserCollection.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace IEnumeratorIEnumerableYield_03.Collection
+{
+    // Создание простой пользовательской коллекции с использованием оператора yield.
+    // Класс, представляющий собой пользовательскую коллекцию.
+    public class UserCollection<T>
+    {
+        readonly T[] elements = new T[4];
+
+        public T this[int index]
+        {
+            get { return elements[index]; }
+            set { elements[index] = value; }
+        }
+
+        int position = -1;
+
+        // Создаем метод Reset().
+        public void Reset()
+        {
+            position = -1;
+        }
+
+        // Создаем метод GetEnumerator(), используем оператор yield.
+        public IEnumerator GetEnumerator()
+        {
+            while (true)
+            {
+                if (position < elements.Length - 1)
+                {
+                    position++;
+                    yield return elements[position];
+                }
+                else
+                {
+                    Reset();
+                    yield break;
+                }
+            }
+
+            //foreach (var element in elements)
+            //{
+            //    yield return element;
+            //}
+
+            //return elements.GetEnumerator();
+        }
+    }
+}
