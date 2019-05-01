d1b3e679d5fd3fd8433a7fa61cf65e1582d6f3fe
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-05-01 22:24:15 +0400

    Создание простой пользовательской коллекции с использованием обобщенных интерфейсов.

---
+++ IEnumeratorIEnumerableYield_02/Collection/Element.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnumeratorIEnumerableYield_02.Collection
+{
+    // Создание простой пользовательской коллекции с использованием обобщенных интерфейсов.
+    // Класс, представляющий собой содержимое коллекции.
+    class Element
+    {
+        public int FieldA { get; set; }
+        public int FieldB { get; set; }
+
+        public Element(int fieldA, int fieldB)
+        {
+            FieldA = fieldA;
+            FieldB = fieldB;
+        }
+    }
+}
---
+++ IEnumeratorIEnumerableYield_02/Collection/UserCollection.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace IEnumeratorIEnumerableYield_02.Collection
+{// Создание простой пользовательской коллекции с использованием обобщенных интерфейсов.
+    // Класс, представляющий собой пользовательскую коллекцию.
+    public class UserCollection<T> : IEnumerable<T>, IEnumerator<T>
+    {
+        readonly T[] elements = new T[4];
+
+        public T this[int index]
+        {
+            get { return elements[index]; }
+            set { elements[index] = value; }
+        }
+
+        int position = -1;
+
+        // Реализация интерфейса IEnumerator<T>:
+        // 1. Метод MoveNext().
+        bool IEnumerator.MoveNext()
+        {
+            if (position < elements.Length - 1)
+            {
+                position++;
+                return true;
+            }
+            return false;
+        }
+
+        // 2. Метод Reset().
+        void IEnumerator.Reset()
+        {
+            position = -1;
+        }
+
+        // 3. Свойство Current.
+        object IEnumerator.Current
+        {
+            get { return elements[position]; }
+        }
+
+        // 4. Обобщенное свойство Current.
+        T IEnumerator<T>.Current
+        {
+            get { return elements[position]; }
+        }
+
+        // Реализация интерфейса IEnumerable<T>:
+        // 1. Метод GetEnumerator().
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this;
+        }
+        // 2. Обобщенный метод GetEnumerator().
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            return this;
+        }
+
+        // Реализация интерфейса IDisposable:
+        // 1. Метод Dispose().
+        void IDisposable.Dispose()
+        {
+            ((IEnumerator)this).Reset();
+        }
+    }
+}
